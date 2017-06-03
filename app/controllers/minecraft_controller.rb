1fbd675c92b5a8e7c435eb3b039c4dd6f697fe9d
Sigurd Kristensen <sikr@nodes.dk>
2017-06-04 01:03:15 +0200

    minecraft fix

--- app/controllers/minecraft_controller.rb
+++ app/controllers/minecraft_controller.rb
@@ -8,7 +8,7 @@ class MinecraftController < ApplicationController
                           :name => 'The Society', # you can put any arbitrary string here
                           :port => 2501,
                           :timeout => 5 # seconds before we give up on the socket connection
-    )
+    ).to_yaml
 
   end
 end
