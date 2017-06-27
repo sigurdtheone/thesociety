7c7c8bed5cf0c0b5e8f147dd541dd2bc49764475
Sigurd Kristensen <sigurd.kristensen@gmail.com>
2017-06-27 19:15:31 +0200

    Update minecraft_api_controller.rb

--- app/controllers/minecraft_api_controller.rb
+++ app/controllers/minecraft_api_controller.rb
@@ -1,17 +1,29 @@
 class MinecraftApiController < ActionController::API
   def show
     begin
+
       @server_info = Glowstone::Server.new("gm.cph.nodescloud.com",
                             :name => 'Nodes', # you can put any arbitrary string here
                             :port => 2500,
                             :timeout => 5 # seconds before we give up on the socket connection
       )
+
+      @server_info.players.each do |player|
+        @players = "#{@players}" + "#{player}\n"
+      end
       
-      render json: @server_info
+      response ={
+          'text' => '',
+          'attachments' => [
+              'color' => 'good',
+              'text' => "*Server*: #{@server_info.name} - #{@server_info.motd}\n*Address*: #{@server_info.host}:#{@server_info.port}\n *Version*: #{@server_info.version}\n *Players*: #{@server_info.num_players}/#{@server_info.max_players}\n *Online*: #{@players}",
+          ]
+      }
+
+      render json: response
       
     rescue Exception
     else
     end
-
   end
 end
