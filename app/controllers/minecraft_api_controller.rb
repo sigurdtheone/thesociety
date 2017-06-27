279d2afac9f152239a7f07fc6cec9f7ee23f77a7
Sigurd Kristensen <sigurd.kristensen@gmail.com>
2017-06-27 19:39:36 +0200

    Update minecraft_api_controller.rb

--- app/controllers/minecraft_api_controller.rb
+++ app/controllers/minecraft_api_controller.rb
@@ -2,7 +2,7 @@ class MinecraftApiController < ActionController::API
   def show
     begin
 
-      @server_info = Glowstone::Server.new("gm.cph.nodescloud.com",
+      @server_info = Glowstone::Server.new('gm.cph.nodescloud.com',
                             :name => 'Nodes', # you can put any arbitrary string here
                             :port => 2500,
                             :timeout => 5 # seconds before we give up on the socket connection
@@ -11,16 +11,29 @@ class MinecraftApiController < ActionController::API
       @server_info.players.each do |player|
         @players = "#{@players}" + "#{player}\n"
       end
-      
-      response ={
-          'text' => '',
+
+      payload ={
+          'channel' => 'testest',
+          'user_name' => "#{params[:user_name]}",
           'attachments' => [
               'color' => 'good',
-              'text' => "*Server*: #{@server_info.name} - #{@server_info.motd}\n*Address*: #{@server_info.host}:#{@server_info.port}\n *Version*: #{@server_info.version}\n *Players*: #{@server_info.num_players}/#{@server_info.max_players}\n *Online*: #{@players}",
+              'title' => "#{@server_info.name} - #{@server_info.motd}",
+              'text' => "*Address*: #{@server_info.host}:#{@server_info.port}\n *Version*: #{@server_info.version}\n *Players*: #{@server_info.num_players}/#{@server_info.max_players}\n *Online*: #{@players}",
           ]
-      }
+      }.to_json
 
-      render json: response
+      uri = URI.parse("#{params[:response_url]}")
+      https = Net::HTTP.new(uri.host,uri.port)
+      https.use_ssl = true
+      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+      req.body = payload
+      res = https.request(req)
+
+      if @res.kind_of? Net::HTTPSuccess
+      else
+        return_error(res)
+      end
       
     rescue Exception
     else
