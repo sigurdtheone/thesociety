e1b688325b483c034d4d110284118dd8f034fb1a
Sigurd Kristensen <sigurd.kristensen@gmail.com>
2018-04-04 23:23:27 +0200

    debug

--- thumb_gen_videos.rb
+++ thumb_gen_videos.rb
@@ -9,18 +9,8 @@ Dir.entries("/app/public/webms/").each do |folder|
         webm_path = "#{@originals}#{webm}"
         webm_destination = "#{@thumbs}#{webm}"
 
-        puts "webm_path: #{webm_path}"
-        puts "webm_destination: #{webm_destination}"
-        puts "webm_destination.sub!: #{webm_destination.sub! '.webm', '.jpg'}"
-
-        webm_destination_sub = webm_destination.sub! '.webm', '.jpg'
-
-        puts webm_path.inspect
-        puts webm_destination.inspect
-        puts webm_destination_sub.inspect
-
-        unless File.file?(webm_destination.sub! '.webm', '.jpg')
-          `ffmpeg -i '#{webm_path}' -ss 00:00:02.000 -vframes 1 '#{webm_destination.sub! '.webm', '.jpg'}'`
+        unless File.file?(webm_destination.sub '.webm', '.jpg')
+          `ffmpeg -i '#{webm_path}' -ss 00:00:02.000 -vframes 1 '#{webm_destination.sub '.webm', '.jpg'}'`
         end
       end
     end
