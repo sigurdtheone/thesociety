662fad5ef2d246bbe3b21f30de4cc120e5e21c6f
Sigurd Kristensen <sigurd.kristensen@gmail.com>
2018-04-04 22:51:30 +0200

    Fixed thumb gen for existing files

--- thumb_gen_videos.rb
+++ thumb_gen_videos.rb
@@ -9,7 +9,7 @@ Dir.entries("/app/public/webms/").each do |folder|
         webm_path = "#{originals}#{webm}"
         webm_destination = "#{thumbs}#{webm}"
 
-        unless File.file?(webm_destination.sub! '.webm', '.jpg')
+        if File.file?(webm_destination.sub! '.webm', '.jpg')
           `ffmpeg -i #{webm_path} -ss 00:00:02.000 -vframes 1 #{webm_destination.sub! '.webm', '.jpg'}`
         end
       end
