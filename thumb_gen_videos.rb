1f469463da5d0bc1f261e960d2082b677b64dbf3
Sigurd Kristensen <sigurd.kristensen@gmail.com>
2018-04-04 21:51:03 +0200

    Fixed thumb gen for webms

--- thumb_gen_videos.rb
+++ thumb_gen_videos.rb
@@ -2,13 +2,10 @@
 
 @webms = Dir.entries("./public/webms/general/original/")
 
-puts "Webms:"
-puts @webms
-
 @webms.each do |webm|
   next if webm  == '.' or webm == '..'
   webm_path = "./public/webms/general/original/#{webm}"
   webm_destination = "./public/webms/general/thumbs/#{webm}"
 
-  `ffmpeg -i #{webm_path} 00:00:02.000 -vframes 1 #{webm_destination.sub! '.webm', '.jpg'}`
+  `ffmpeg -i #{webm_path} -ss 00:00:02.000 -vframes 1 #{webm_destination.sub! '.webm', '.jpg'}`
 end
